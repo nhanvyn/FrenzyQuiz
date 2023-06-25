7086bb3b2c98bc920bfe50d266d6a320d2e11b70
NhVNgn <limou@LAPTOP-2EC8MA82>
2023-06-24 19:42:19 -0700

    added navbar

---
+++ client/src/comps/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">FrenzyQuiz</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink activeclassname="active" className="nav-link" to="/">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeclassname="active" className="nav-link" to="/">
+                Register
+              </NavLink>
+            </li>
+         
+        
+          </ul>
+        </div>
+      </nav>
+      
+    </>
+  )
+};
+
+export default Navbar;
