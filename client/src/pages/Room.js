75aca417d2b94c23ea6d1d00218016ddedc702ae
NhVNgn <limou@LAPTOP-2EC8MA82>
2023-07-10 22:10:25 -0700

    move user to homepage if waiting room is deleted

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import QRCode from 'qrcode.react';
-import { SocketContext } from '../App';
+import { SocketContext, UserContext } from '../App';
+import { useNavigate } from 'react-router-dom';
 
 const Room = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [quiz, setQuiz] = useState(null);
   const [players, setPlayers] = useState([])
   const socket = useContext(SocketContext)
-
+  const user = useContext(UserContext)
 
   // this hook is for fetching quiz information
   useEffect(() => {
@@ -18,30 +20,52 @@ const Room = () => {
   }, [id])
 
 
-
   useEffect(() => {
-    console.log("Join room called")
-    socket.emit("join_room", { quizId: id, username: "someuser" })
-    
-    // disconnect player from the room when component is unmounted
-    return () => {
-      console.log("Leave room called")
-      socket.emit('leave_room', { quizId: id })
+    if (socket){
+      const user_email = user? user.email : "Anonymous"
+
+      socket.emit("join_room", { quizId: id, email: user_email })
+      
+
     }
-  }, []);
+  
+  }, [socket, id, user]);
+
+
 
 
   useEffect(() => {
     if (socket) {
       console.log("socketID: ", socket.id)
+  
       const handleDisplayPlayer = (data) => {
         console.log("display player:", data);
         setPlayers(data)
       };
+
+      const handleUpdateQuiz = (data) => {
+        console.log("display quiz:", data)
+        setQuiz(data)
+      }
+
+      const handleRoomDeleted = (data) => {
+        console.log("this room is deleted")
+        navigate(`/`)
+
+
+      }
+
       socket.on("display_new_player", handleDisplayPlayer);
+
+      socket.on("update_quiz", handleUpdateQuiz);
+
+      socket.on("room_deleted", handleRoomDeleted);
+
 
       return () => {
         socket.off("display_new_player", handleDisplayPlayer);
+        socket.off("update_quiz", handleUpdateQuiz);
+        socket.off('room_deleted');
         console.log("Called")
       };
     }
@@ -52,7 +76,7 @@ const Room = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-6 col-md-12">
-          <h3>Quiz id: {id}</h3>
+          <h3> {"Quiz id:" + quiz?.id + " - " + " question: " + quiz?.question}</h3>
           <QRCode value={`http://localhost:3000/Room/${id}`} size={256*2} />
         </div>
 
@@ -60,7 +84,7 @@ const Room = () => {
           <h3>Wait for player to join...</h3>
           <ul>
             {players.map((player, index) => (
-              <li key={index}>{"username: " + player.username + " - connect id: " + player.connect_id}</li>
+              <li key={index}>{"user: " + player.email + " - socket id: " + player.connect_id}</li>
             ))}
           </ul>
         </div>
